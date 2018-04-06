1f96f2ba40d74200ff58d2d0cf8758926ae78354
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2018-04-06 19:21:42 +0800

    created constant ingame file list

---
+++ Assets/Scripts/Utilities/Constants/IngameFileList.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// Contains file paths for every ingame-related asset.
+/// </summary>
+public class IngameFileList
+{
+    public const string INGAME_FILE_PATH = "Prefabs/Ingame/";
+
+    public const string INGAME_TABLE_PREFAB_PATH = INGAME_FILE_PATH + "Table/Table_Flip";
+
+}
