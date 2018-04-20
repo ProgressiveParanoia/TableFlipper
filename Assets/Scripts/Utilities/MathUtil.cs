a17916555e6db88bf6d6724c83050bf5e0ed310a
Jed Lyle Fernandez <jedeyenyt654@gmail.com>
2018-04-20 18:15:57 +0800

    added math utility

---
+++ Assets/Scripts/Utilities/MathUtil.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ShadowMonsters.Utilities
+{
+    public class MathUtil
+    {
+        public static float Normalize(float currentValue, float maxValue)
+        {
+            return currentValue / maxValue;
+        }
+    }
+}
